821e12c84562fd99b8522d97b3e6e288c9f32800
Chandrika Sista <cguntur@gmail.com>
2024-10-22 09:40:53 -0700

    Added the user model

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
+//import mongoose
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/myRecipes');
+//Connection string
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/myRecipes';
+
+//Connect to MongoDB
+mongoose.connect(connectionString);
 
 module.exports = mongoose.connection;
